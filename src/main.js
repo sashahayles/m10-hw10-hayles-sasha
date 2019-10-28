34a1e8b60b9cd4dd32a7207b7b0e78c6cfa3a89c
Sasha Hayles <sashahayles@yahoo.com>
2019-10-27 21:09:13 -0400

    Add jS script arrow function and set timeout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
-// It is always helpful to use comments in your code!
+//boiling
+const boiling = document.getElementById('boiling-temp');
+var boilingtxt = document.createTextNode ("212");
+boiling.appendChild (boilingtxt);
+//freezing
+const freezing = document.getElementById ('freezing-temp');
+var freezingtxt = document.createTextNode ("32");
+freezing.appendChild (freezingtxt);
+//random water temp
+let randomtemp = document.getElementById ('water-temp');
+let temptxt = document.createTextNode ("50");
+randomtemp.appendChild (temptxt);
+
+let goodTemp = document.getElementById ('good-temp');
+setTimeout(()=> {
+  goodTemp.classList.remove('hide');
+  goodTemp.classList.add('show');
+}, 3000);
+
+
+//extra 
+let showgoodtemp = () => {
+randomtemp.classList.remove('hide');
+randomtemp.classList.add('show');
+};
+
+setTimeout(showgoodtemp, 3000);
+//setTimeout( function () {
+  //randomtemp.classList.remove ('hide');
+  //randomtemp.classList.add ('show');
+//}, 3000);
